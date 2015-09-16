94093fb9eb32bce82fd712d532e31f3f0b9ee8f1
KazuyaMIURA <miup.0627@gmail.com>
2015-09-16 11:34:54 +0900

    fix layout

--- Inspire/controller/DetailViewController.swift
+++ Inspire/controller/DetailViewController.swift
@@ -5,21 +5,30 @@ import CoreMedia
 
 class DetailViewController:ISPViewController{
 
-    @IBOutlet weak private var artwork: UIImageView!
+    @IBOutlet weak private var blurImageView: UIImageView!
+    @IBOutlet weak private var secondImageView: UIImageView!
+    @IBOutlet weak private var thirdImageView: UIImageView!
+    @IBOutlet weak private var firstImageView: UIImageView!
     @IBOutlet weak private var trackTime: UILabel!
     @IBOutlet weak private var playTime: UILabel!
     @IBOutlet weak private var trackTitle: UILabel!
     @IBOutlet weak private var artist: UILabel!
-    @IBOutlet weak var playTimeSlider: UISlider!
+    @IBOutlet weak private var playTimeSlider: UISlider!
+    private let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .Dark))
 
-    var playingTimeTimer : NSTimer?
+    private var playingTimeTimer : NSTimer?
     var playlist: Playlist?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupView()
+        setupView()
         PlayerManager.sharedInstance.setPlaylist(playlist!)
-        self.updatePlayingInfo()
+        updatePlayingInfo()
+        title = ""
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        playingTimeTimer?.invalidate()
     }
 
     @IBAction func playAndPauseButtonTouchUpInside(sender: UIButton) {
@@ -39,28 +48,60 @@ class DetailViewController:ISPViewController{
     }
 
     @IBAction func playTimeSliderAction(sender: AnyObject) {
-        PlayerManager.sharedInstance.setProgress(self.playTimeSlider.value)
+        playingTimeTimer?.invalidate()
     }
 
-    func setupView(){
-        self.playTimeSlider.maximumValue = 1.0
 
+    @IBAction func endEditing(sender: AnyObject) {
+        PlayerManager.sharedInstance.setProgress(self.playTimeSlider.value)
         playingTimeTimer = NSTimer.scheduledTimerWithTimeInterval(
-            1,
+            0.01,
             target: self,
             selector: "updatePlayingInfo",
             userInfo: nil,
             repeats: true
         )
+        playingTimeTimer?.fire()
+    }
+
+    func setupView(){
+        blurView.frame = UIScreen.mainScreen().bounds
+        blurImageView.addSubview(blurView)
+        self.playTimeSlider.maximumValue = 1.0
+
+        playingTimeTimer = NSTimer.scheduledTimerWithTimeInterval(
+            0.01,
+            target: self,
+            selector: "updatePlayingInfo",
+            userInfo: nil,
+            repeats: true
+        )
+        playTimeSlider.setThumbImage(UIImage(named: "SliderThumb"), forState: .Normal)
+        playTimeSlider.setThumbImage(UIImage(named: "SliderThumb"), forState: .Highlighted)
+        playTimeSlider.tintColor = UIColor(red: 128/255, green: 128/255, blue: 227/255, alpha: 1)
     }
 
     func updatePlayingInfo() {
-        self.trackTime.text = PlayerManager.sharedInstance.playTime() ?? "00:00"
-        self.playTime.text = PlayerManager.sharedInstance.playingTime() ?? "00:00"
-        self.playTimeSlider.value = PlayerManager.sharedInstance.getProgress() ?? Float(0.0)
+        trackTime.text = PlayerManager.sharedInstance.playTime() ?? "00:00"
+        playTime.text = PlayerManager.sharedInstance.playingTime() ?? "00:00"
+        playTimeSlider.value = PlayerManager.sharedInstance.getProgress() ?? Float(0.0)
 
-        self.trackTitle.text = PlayerManager.sharedInstance.title() ?? "TITLE"
-        self.artist.text = PlayerManager.sharedInstance.artist() ?? "ARTIST"
-        self.artwork.sd_setImageWithURL(PlayerManager.sharedInstance.artworkUrl())
+        trackTitle.text = PlayerManager.sharedInstance.title() ?? "TITLE"
+        artist.text = PlayerManager.sharedInstance.artist() ?? "ARTIST"
+        firstImageView.sd_setImageWithURL(PlayerManager.sharedInstance.artworkUrl())
+        blurImageView.sd_setImageWithURL(PlayerManager.sharedInstance.artworkUrl())
+        let numberOfSongs = playlist?.musicTracks.count
+        let toPlayIndex = PlayerManager.sharedInstance.toPlayIndex
+        switch PlayerManager.sharedInstance.toPlayIndex {
+            case (numberOfSongs! - 2):
+                secondImageView.sd_setImageWithURL(NSURL(string: playlist?.musicTracks[toPlayIndex + 1].artworkUrl ?? ""))
+                thirdImageView.sd_setImageWithURL(NSURL(string: playlist?.musicTracks[0].artworkUrl ?? ""))
+            case (numberOfSongs! - 1):
+                secondImageView.sd_setImageWithURL(NSURL(string: playlist?.musicTracks[0].artworkUrl ?? ""))
+                thirdImageView.sd_setImageWithURL(NSURL(string: playlist?.musicTracks[1].artworkUrl ?? ""))
+            default:
+                secondImageView.sd_setImageWithURL(NSURL(string: playlist?.musicTracks[toPlayIndex + 1].artworkUrl ?? ""))
+                thirdImageView.sd_setImageWithURL(NSURL(string: playlist?.musicTracks[toPlayIndex + 2].artworkUrl ?? ""))
+        }
     }
 }
