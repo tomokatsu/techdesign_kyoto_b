ac15ae8ba379962db53789e9d796062f0f922677
KazuyaMIURA <miup.0627@gmail.com>
2015-09-17 13:05:55 +0900

    nits

--- Inspire/controller/PublishViewController.swift
+++ Inspire/controller/PublishViewController.swift
@@ -75,6 +75,10 @@ class PublishViewController: ISPViewController, UITableViewDataSource, UITableVi
         return playlist?.musicTracks.count ?? 0
     }
 
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat.min
+    }
+
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 80
     }
